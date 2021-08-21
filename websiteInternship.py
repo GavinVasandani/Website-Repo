9757e31099f2ad58e6dfdb9b63cad62e88f1c324
GavinVasandani <gavinvasandani.2002@gmail.com>
2021-08-21 21:01:59 +0400

    Second commit

--- websiteInternship.py
+++ websiteInternship.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
 
-    st.write("Hello website! My name is Gavin! This is an update to the commit. Does github connect to VS Code? Please!");
+    st.write("Hello website! V1");
 
 if __name__ == "__main__":
     main();
