e3f3b7b0dcf0f1abd66feedfaf74e2e6e01d23be
GavinVasandani <gavinvasandani.2002@gmail.com>
2021-08-21 20:59:31 +0400

    Website V1

---
+++ README.md
@@ -0,0 +1,1 @@
+# WebIntern2
---
+++ websiteInternship.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+
+    st.write("Hello website! My name is Gavin! This is an update to the commit. Does github connect to VS Code? Please!");
+
+if __name__ == "__main__":
+    main();
+
+
+
